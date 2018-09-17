8f43328bf241232879f265e090e0e178de453547
Вася Пупкин <vasya@gmail.com>
2018-09-17 20:34:32 +0300

    new

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,21 +3,15 @@
 namespace App\Http\Controllers;
 
 namespace App\Http\Controllers;
- use App\Description;
-use App\Http\Controllers\Controller;
-//use Illuminate\Http\Request;
-use app\Post;
-use App\Home;
-use App\TeamUser;
-use App\User;
-use App\Team;
-use App\Email;
-use App\HomeDec;
-//use Illuminate\Support\Facades\DB;
-use  Illuminate\Support\Facades\Validator;
 
+use App\Description;
+use App\HomeDec;
+use App\Team;
+use App\TeamUser;
 use Illuminate\Http\Request;
-use Carbon\Carbon;
+
+//use Illuminate\Http\Request;
+//use Illuminate\Support\Facades\DB;
 
 
 class HomeController extends Controller
@@ -39,7 +33,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-//        $this->middleware('auth');
+        $this->middleware('auth');
     }
 
     public function index()
@@ -49,6 +43,7 @@ class HomeController extends Controller
 
         // показывает  дату и сами ближайшие игры которые состоятся
         $tasksNear = Team::orderBy('game_date', 'asc')->where('game_date', '>=', date('Y-m-d H:i:s'))->take(5)->get();
+//        $tasksNear = Team::all();
 //        $users1 = User::all();
         return view('home.index', [
             'tasksAll' => $tasksAll,
@@ -56,22 +51,22 @@ class HomeController extends Controller
         ]);
     }
 
-    public function show($id , Request $request)
+    public function show($id, Request $request)
     {      // ПЕРЕХОДИТЬ НА ID ЭТОЙ ТАЛИЦЫ    //И ВЫВОДИТ ОПИСАНЕЕ О КОМАНДЕ
-        echo "<br>" . " это статья номер  " .$id;
+        echo "<br>" . " это статья номер  " . $id;
 
         $counts = Team::all();
-        $teamusers  = TeamUser::all();
+        $teamusers = TeamUser::all();
         $commands = TeamUser::where('teams_id', '=', $id)->get();
 
 
         return view('home.team', [
-                                    'descrip' => Team::findOrFail($id),
-                                    'teams' => TeamUser::where('teams_id', '=', $id)->first(),
-                                    'commands' => $commands,
-                                    'counts' => $counts,
-                                    'teamusers' => $teamusers,
-                                    ]); // descrip  МОЖНО ИСПОЛЬЗОВАТЬ ЧТО БЫ ВЫЗВАТЬ ИЗ ТАБЛИЦЫ НУЖНЫЕ ДАННЫЕ ОБРАЩАЯСЬ ЧЕРЕЗ ДОПУСТИМ  {!! $news->id !!} ИЛИ ЧЕРЕЗ fOREACH
+            'descrip' => Team::findOrFail($id),
+            'teams' => TeamUser::where('teams_id', '=', $id)->first(),
+            'commands' => $commands,
+            'counts' => $counts,
+            'teamusers' => $teamusers,
+        ]); // descrip  МОЖНО ИСПОЛЬЗОВАТЬ ЧТО БЫ ВЫЗВАТЬ ИЗ ТАБЛИЦЫ НУЖНЫЕ ДАННЫЕ ОБРАЩАЯСЬ ЧЕРЕЗ ДОПУСТИМ  {!! $news->id !!} ИЛИ ЧЕРЕЗ fOREACH
     }
 
     public function team()
@@ -93,27 +88,26 @@ class HomeController extends Controller
 //            echo $home . '<br>';
 //        }
         foreach ($homes as $home) {
-            echo $home->id. '<br>';
+            echo $home->id . '<br>';
         }
         echo view('home.team', ['teams' => $teams]);
     }
 
 
-
-    public function create($id , Request $request)
+    public function create($id, Request $request)
     {
         // ПЕРЕХОДИТЬ НА ID ЭТОЙ ТАЛИЦЫ      //И ВЫВОДИТ ОПИСАНЕЕ О КОМАНДЕ
-        echo "<br>" . " это статья номер  " .$id;
+        echo "<br>" . " это статья номер  " . $id;
 
-        $commands = TeamUser::find( $id);
+        $commands = TeamUser::find($id);
 //        $commands = TeamUser::find(1)->user()->where('user_id')->first();
         $commands = $commands->user;
         $counts = Team::all();
-        $teamusers  = TeamUser::all();
+        $teamusers = TeamUser::all();
 
         //  сохранение подписчиков которые добавляются в команду
         $teamuser = new TeamUser();
-         $teamuser->teams_id = $request->teams_id;
+        $teamuser->teams_id = $request->teams_id;
         $teamuser->user_id = $request->user_id;
         $teamuser->save();
         return view('home.team', [
@@ -127,22 +121,22 @@ class HomeController extends Controller
     }
 
 
-    public function delete( Request $request , $id )
+    public function delete(Request $request, $id)
     {
-        $commands = TeamUser::find( $id);
+        $commands = TeamUser::find($id);
 //        $commands = TeamUser::find($id)->user()->where('user_id')->first();
 //        if( $commands = $commands->user) {
 //            continion;
 //        }
 
 //        $commands = $commands->user;
-        $teamusers  = TeamUser::all();
+        $teamusers = TeamUser::all();
         //  сохранение подписчиков которые добавляются в команду
         $teamuser = new TeamUser();
         $teamuser->teams_id = $request->teams_id;
         $teamuser->user_id = $request->user_id;
         $teamuser->save();
-        $a =\Auth::id();
+        $a = \Auth::id();
 
         // удаляет с таблицы team_user ид юзера и номер команды на которую он подписался--}}
 //        if (\App\TeamUser::where('user_id', '=', $a) and
@@ -156,10 +150,10 @@ class HomeController extends Controller
 //        }
 
         \App\TeamUser::where('user_id', '=', $a)->
-                where('teams_id', '=', \Request::input('teams_id'))->delete();
+        where('teams_id', '=', \Request::input('teams_id'))->delete();
 
 
-        return  view('home.team', [
+        return view('home.team', [
             'descrip' => Team::findOrFail($id),
             'teams' => TeamUser::where('teams_id', '=', $id)->get(),
             'teamusers' => $teamusers,
@@ -168,11 +162,10 @@ class HomeController extends Controller
     }
 
 
-
     public function date()
     {
         // ПОКА НЕ ЗАДЕЙСТВОВАНА НИГДЕ
-        echo "<br>" . " это статья номер  " ;
+        echo "<br>" . " это статья номер  ";
         $date = date('Y');
 //                                    $tasks = Description::all();
 //                                        //            dd($users);
